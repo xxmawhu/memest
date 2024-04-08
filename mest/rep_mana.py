56f12bfac2294fb20b57d992ea213d6b27a13e8e
Ma Xin-Xin <xxmawhu@163.com>
2024-04-08 14:51:25 +0800

    update

--- mest/rep_mana.py
+++ mest/rep_mana.py
@@ -1,7 +1,19 @@
+import time
+
+from loguru import logger
+
+
 class MeST:
 
     def __init__(self, cfg):
-        pass
+        self.cfg = cfg
 
     def check(self):
-        pass
+        if self.cfg.update_config():
+            all_rep = [i for i in self.cfg.get_sections() if i != "default"]
+            logger.info("all_rep: {}", all_rep)
+
+    def run(self):
+        while True:
+            self.check()
+            time.sleep(5)
