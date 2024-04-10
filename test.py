048cdd72962dbc608e0d4117cc66f24afdba4d0a
Ma Xin-Xin <xxmawhu@163.com>
2024-04-10 13:03:53 +0800

    update

--- test.py
+++ test.py
@@ -1,3 +1,16 @@
-from mest import main
+import hashlib
 
-main.run_forever()
+def unique_12_char_string(input_str):
+    # 使用MD5进行哈希计算
+    hash_object = hashlib.md5(input_str.encode())
+    hex_dig = hash_object.hexdigest()
+
+    # 取前12位作为唯一标识符
+    unique_id = hex_dig[:12]
+
+    return unique_id
+
+# 测试
+input_str = "This is a long string be hashed."
+result = unique_12_char_string(input_str)
+print(result)
